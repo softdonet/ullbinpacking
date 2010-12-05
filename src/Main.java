dc08b396e1128c8cda9bd816c9ee667e82b6fa9c
lcdsound <lcdsound@38a6248b-f478-8f4b-3c61-6e526adf4029>
2010-12-05 23:07:53 +0000

    primera heuristica hecha

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import app.Problema;
+import app.Heuristica;
 
 public class Main
 {
 	public static void main (String args[])
 	{
-		Problema p = new Problema ("./datasets/mod3.dat");
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
 		
-		System.out.println (p);
-		System.out.println (p.getSolucion ());
+		System.out.println("Proyecto Heuristicas 2010/11: Non-guillotine two-dimensional bin packing problem");
+		System.out.println();
+		System.out.println();
+		System.out.println("Problema a resolver:");
+		System.out.println();
+		
+		Problema p = new Problema("./datasets/modelo.dat");
+		System.out.println(p);
+		
+		System.out.println("Introduzca la heuristica que desea utilizar para obtener una solucion optima");
+		System.out.println("Busquedas por entornos:");
+		System.out.println("0 - BAP: Busqueda aleatoria pura");
+		//System.out.println("1 - NVeces: Ejecutar N-veces");
+		//System.out.println();
+		System.out.print("Opcion: ");
+		int opcion = 0;
+		try {
+			opcion = Integer.parseInt(br.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println();
+		Heuristica h = new Heuristica(opcion, p);
+		
 	}
 }
